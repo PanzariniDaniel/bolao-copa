7720da97d141b014b0f7bda729d9019d78679839
Daniel Panzarini <daniel.panzarini@rw3tecnologia.com>
2022-11-03 00:25:58 -0300

    Criando rotas de contagem de guesses e users e criando novos pools

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({ log: ["query"] });
 
@@ -11,6 +13,34 @@ async function bootstrap() {
 
   fastify.get("/pools/count", async () => {
     return { count: await prisma.pool.count() };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code,
+      },
+    });
+
+    return reply.status(201).send({ code });
+  });
+
+  fastify.get("/users/count", async () => {
+    return { count: await prisma.user.count() };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    return { count: await prisma.guess.count() };
   });
 
   await fastify.listen({ port: 3333 /*host: "0.0.0.0"*/ });
