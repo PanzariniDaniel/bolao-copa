f6d4ddcc26eadf4dac7d9a525309fd3ba034309c
Daniel Panzarini <daniel.panzarini@rw3tecnologia.com>
2022-11-05 00:17:36 -0300

    Criando as interfaces de new, find e pools

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,9 @@ import { THEME } from "./src/styles/theme";
 
 import { Loading } from "./src/components/Loading";
 import { SignIn } from "./src/screens/SignIn";
+import { New } from "./src/screens/New";
+import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
@@ -28,7 +31,7 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
