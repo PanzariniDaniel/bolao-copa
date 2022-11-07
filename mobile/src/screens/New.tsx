ed01a30b208b6ca7d2a54cd90f517fd6f8e3a130
Daniel Panzarini <daniel.panzarini@rw3tecnologia.com>
2022-11-06 21:15:39 -0300

    Criando a funcionalidade de criar um bolão pelo app

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,11 +1,47 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, Text, useToast, VStack } from "native-base";
 import { Header } from "../components/Header";
 
 import Logo from "../assets/logo.svg";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from "react";
+import { api } from "../services/api";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  async function handlePoolCreate() {
+    if (!title.trim()) {
+      setTitle("");
+      return toast.show({
+        title: "Informe um nome para o seu bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+    setIsLoading(true);
+    try {
+      await api.post("/pools", { title });
+      setTitle("");
+      return toast.show({
+        title: "Bolão criado com sucesso!",
+        placement: "top",
+        bgColor: "green.500",
+      });
+    } catch (error) {
+      console.log(error);
+      return toast.show({
+        title: "Não foi possível criar o bolão",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -20,12 +56,21 @@ export function New() {
           my={8}
           textAlign="center"
         >
-          Crie seu próprio bolão da copa {'\n'} e compartilhe entre amigos!
+          Crie seu próprio bolão da copa {"\n"} e compartilhe entre amigos!
         </Heading>
 
-        <Input mb={2} placeholder="Qual o nome do seu bolão" />
+        <Input
+          mb={2}
+          placeholder="Qual o nome do seu bolão"
+          value={title}
+          onChangeText={setTitle}
+        />
 
-        <Button title="CRIAR MEU BOLÃO" />
+        <Button
+          title="CRIAR MEU BOLÃO"
+          onPress={handlePoolCreate}
+          isLoading={isLoading}
+        />
 
         <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que poderá usar
